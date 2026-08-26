4fa4b09eabe244d7d3e17d7443c9079ea0c17c8c
Omar Zabadani <omarzabadani003@gmail.com>
2026-08-26 19:45:59 +0300

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-import zipfile
-
-zip_path = r"D:\download\training_AI\weak 7 and 8\dataset\TechStore_AI_Agent_Training_Data.zip"
-
-with zipfile.ZipFile(zip_path, "r") as zip_ref:
-    print(zip_ref.namelist())
-
